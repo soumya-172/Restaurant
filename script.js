7539fbed18068c1df396c332d6f3928d1a78218e
Soumya <amiyo282@gmail.com>
2023-04-10 23:34:58 +0530

    uploading the project

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('#menu-bar').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('#menu-bar').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        //scroll spy
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let offset = $(this).offset().top-200;
+            let height = $(this).height();
+            let id = $(this).attr('id');
+
+            if(top > offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+    });
+
+    $('.list .btn').click(function(){
+         $(this).addClass('active').siblings().removeClass('active');
+
+         let src = $(this).attr('data-src');
+
+         $('.menu .row .image img').attr('src', src);
+    });
+});
